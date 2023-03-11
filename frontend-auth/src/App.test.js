ef18275be83ad23e439de8350be9e3220deaac6c
Alexander Indenbaum <alexander.indenbaum@gmail.com>
2023-03-11 13:27:50 +0200

    Add node.js workflow - https://github.com/actions/starter-workflows/tree/main/ci

--- frontend-auth/src/App.test.js
+++ frontend-auth/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders login message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Log in with your account to continue./i);
   expect(linkElement).toBeInTheDocument();
 });
